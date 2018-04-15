eb78b4cb0c147145a2eed015d5bf00f03924d2a8
SandroCourses <sandroneumann@me.com>
2018-04-15 15:59:46 +0200

    Third version of project

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,18 +1,54 @@
-const printBoard = (board) => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+
+  for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+
+    for(let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(' ');
+    }
+
+    board.push(row);
+  }
+
+  return board;
 };
 
-let board = [];
-board.push([' ', ' ', ' ']);
-board.push([' ', ' ', ' ']);
-board.push([' ', ' ', ' ']);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
 
-printBoard(board);
+  for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
 
-board[0][1] = '1';
-board[2][2] = 'B';
+    for(let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(null);
+    }
 
-printBoard(board);
+    board.push(row);
+  }
+
+  let numberOfBombsPlaced = 0;
+
+  while(numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+    // It's possible that this will set a bomb where a bomb already exists
+    board[randomRowIndex][randomColumnIndex] = 'B';
+
+    numberOfBombsPlaced++;
+  }
+
+  return board;
+};
+
+const printBoard = board => console.log(board.map(row => row.join(' | ')).join('\n'));
+
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+
+console.log('Bomb Board: ');
+printBoard(bombBoard);
